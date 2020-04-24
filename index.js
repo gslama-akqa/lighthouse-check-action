6db6d2fa5d404a39fc656af790bd68115a5e5b26
Gabriel Slama <gabriel.slama@akqa.com>
2020-04-24 17:09:31 +0200

    Add auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const get = require('lodash.get');
 const github = require('@actions/github');
 const { lighthouseCheck } = require('@foo-software/lighthouse-check');
+const puppeteer = require("puppeteer");
 
 const formatInput = input => {
   if (input === 'true') {
@@ -22,6 +23,24 @@ const formatInput = input => {
 (async () => {
   try {
     const urls = formatInput(core.getInput('urls'));
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(urls[0]);
+    await page.waitForSelector('input[type="password"]');
+    const passwordInput = await page.$('input[type="password"]');
+    await passwordInput.type("akqa2020");
+    await Promise.all([
+      page.$eval("form", (form) => form.submit()),
+      page.waitForNavigation(),
+    ]);
+    const alterBox = await page.$('.alert-box');
+    if (alterBox !== null) {
+        throw await alterBox.evaluate(node => node.innerText)
+    }
+    await page.close();
+    await browser.close();
+    
     const extraHeaders = core.getInput('extraHeaders');
     const prApiUrl = get(github, 'context.payload.pull_request.url');
 
